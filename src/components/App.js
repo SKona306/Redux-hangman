b706b9c57b954e9f85c4fe2ba051400269786dba
Shaun Kent <shaunkent81@gmail.com>
2022-01-25 19:45:03 -0800

    pass Header and GameControl component to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Header from '../components/Header';
+import GameControl from '../components/GameControl';
 
 function App() {
   return (
     <React.Fragment>
-    
+      <Header />
+      <GameControl />
     </React.Fragment>
   );
 }
