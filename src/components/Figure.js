f73ad0b79d7ba1c21cbdac0fcf216e7185a6f6de
Shaun Kent <shaunkent81@gmail.com>
2022-01-25 19:52:50 -0800

    create figure component and add logic to show images and wrong guesses counter

---
+++ src/components/Figure.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Figure = (props) => {
+  return (
+  <React.Fragment>
+    <div className='figure' style={{display: "flex", justifyContent: "space-evenly"}}>
+      <div className='images'>
+        <img src={props.images[props.mistakes]}/>
+      </div>
+      <div className='wrong-guess-tracker' style={{padding: "2rem", paddingLeft: "5rem"}}>
+        <h4>Wrong Guesses:</h4>
+        <p>{props.mistakes} of 6</p>
+      </div>
+    </div>
+    <hr />
+    
+    
+  </React.Fragment>);
+};
+
+export default Figure;
