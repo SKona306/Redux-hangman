518624eb737fc3b584e91c19b14b445a73a97ebe
Shaun Kent <shaunkent81@gmail.com>
2022-01-26 19:17:06 -0800

    add logic to display guessable letters

--- src/components/GuessableLetters.js
+++ src/components/GuessableLetters.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 const GuessableLetters = (props) => {
   const letters = "abcdefghijklmnopqrstuvwxyz";
-  const { onClickingLetter, guessed } = props;
+  const { onClickingLetter, gameState} = props;
+  const editedPhrase = gameState.answer.replaceAll(" ", "");
 
   return (
   <React.Fragment>
     <hr />
-    {letters.split('').map(letter => (
-      <button
-        key={letter}
-        value={letter}
-        onClick={() => onClickingLetter(letter)}
-        disabled={guessed.includes(letter)}
-        >{letter}</button>
-    ))}
+    <div className='letters' style={{display: "flex", alignItems: 'center', justifyContent: 'center'}}>
+      {letters.split('').map(letter => (
+        <button
+          key={letter}
+          value={letter}
+          onClick={() => onClickingLetter(letter)}
+          disabled={gameState.guessed.includes(letter) || gameState.correct === editedPhrase.length || gameState.mistake === 6}
+          >{letter}</button>
+      ))}
+    </div>
+    
   </React.Fragment>
   );
 };
