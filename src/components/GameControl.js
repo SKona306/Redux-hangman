6fdb44a13d04c44a049f64aa79c83199c66cc6b9
Shaun Kent <shaunkent81@gmail.com>
2022-01-26 19:16:39 -0800

    add logic for win condition and reset button

--- src/components/GameControl.js
+++ src/components/GameControl.js
@@ -9,6 +9,7 @@ import step3 from '../images/3.jpg';
 import step4 from '../images/4.jpg';
 import step5 from '../images/5.jpg';
 import step6 from '../images/6.jpg';
+import Results from './Results';
 
 class GameControl extends React.Component {
 
@@ -20,6 +21,7 @@ class GameControl extends React.Component {
     super(props);
     this.state = {
       mistake: 0,
+      correct: 0,
       guessed: [],
       answer: ''
     }
@@ -30,18 +32,21 @@ class GameControl extends React.Component {
     this.setState(st => ({
       answer: st.answer = phrase
     }));
+    console.log(this.state);
   }
 
   handleGuess = e => {
     this.setState (st => ({
       guessed: st.guessed.concat(e),
-      mistake: st.mistake + (this.state.answer.includes(e) ? 0 : 1)
+      mistake: st.mistake + (this.state.answer.includes(e) ? 0 : 1),
+      correct: st.correct + (this.state.answer.includes(e) ? 1 : 0)
     }));
   }
 
   handleReset = () => {
     this.setState({
       mistake: 0,
+      correct: 0,
       guessed: [],
       answer: ''
     })
@@ -49,19 +54,30 @@ class GameControl extends React.Component {
 
 
   render() {
+    const resetButton = (this.state.mistake > 5) ? <button style={{marginLeft: "3em"}} onClick={this.handleReset}>New Phrase</button> : ""
     const startButton = (this.state.answer !== '') ? "" : <button onClick={this.handleGetPhrase}>Generate</button>
     return(
       <React.Fragment>
         <Figure 
           mistakes = {this.state.mistake}
           images = {this.props.images}/>
-        {startButton}
+        <div className='generate' style={{display: "flex", alignItems: 'center', justifyContent: 'center'}}>
+          {startButton}
+        </div>
         <Phrase
           phrase = {this.state.answer}
           guessed = {this.state.guessed}/>
         <GuessableLetters 
           onClickingLetter = {this.handleGuess} 
-          guessed = {this.state.guessed}/>
+          gameState = {this.state}/>
+        <Results
+          answer = {this.state.answer}
+          mistakes = {this.state.mistake}
+          correct = {this.state.correct}/>
+          <div className='reset' style={{display: "flex", alignItems: 'center', justifyContent: 'center', marginTop: "2rem"}}>
+            {resetButton}
+          </div>
+          
       </React.Fragment>
     )
   }
